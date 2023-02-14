85199b42667cb03e3a4d59580b87f4c753f62a22
Andreas Murrell <andreas@merl.us>
2023-02-14 12:23:01 -0800

    added debounce but not working

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,17 @@ int yellow = 9;
 int green = 6;
 int blue = 5;
 int red = 3;
+
 int photoResistor = A0;
 int buttonPin = 2;
 int buttonState = 0;
 int pattern = 0;
+
+// debounce
+
+byte lastButtonState;
+unsigned long lastButtonStateChanged = millis();
+unsigned long debounceDeration = 50; // milis
 
 void allOff()
 {
@@ -19,29 +26,43 @@ void allOff()
 
 void setup()
 {
+  Serial.begin(9600);
   pinMode(yellow, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(blue, OUTPUT);
   pinMode(red, OUTPUT);
   pinMode(photoResistor, INPUT); // setting photo resistor as input
   pinMode(buttonPin, INPUT);
+  // debounce
+  lastButtonState = digitalRead(buttonPin);
 }
 
 void loop()
 {
-  Serial.println(buttonState);
-  buttonState = digitalRead(buttonPin);
-
-  if (buttonState == HIGH)
+  // debounce
+  if (millis() - lastButtonStateChanged >= debounceDeration)
   {
-    
-    //delay(10); // add debounce delay
-    if (buttonState == HIGH)
+    byte buttonStateDebounce = digitalRead(buttonPin);
+    if (buttonStateDebounce != lastButtonState)
     {
-       allOff();
-      pattern = (pattern + 1) % 3;
+      lastButtonStateChanged = millis();
+      lastButtonState = buttonStateDebounce;
+      if (buttonStateDebounce == LOW)
+      {
+        Serial.println("button has been relesed");
+
+      }
     }
   }
+
+  //Serial.println(buttonState);
+  buttonState = digitalRead(buttonPin);
+
+    if (buttonState == HIGH)
+    {
+      allOff();
+      pattern = (pattern + 1) % 3;
+    }
 
   if (pattern == 0)
   {
@@ -54,33 +75,33 @@ void loop()
   else if (pattern == 1)
   {
     int photoResistorStatus = analogRead(photoResistor); // read the status of photo resistor
-      int ledVal = map(photoResistorStatus, 300, 800, 0, 200);
-      Serial.println(photoResistorStatus);
-      Serial.println(ledVal);
-      Serial.println("case 2 is working");
-      if (photoResistorStatus <= 450)
-      {
-        analogWrite(yellow, 5);
-        delay(100);
-        analogWrite(yellow, LOW);
-        delay(100);
-        analogWrite(green, ledVal);
-        delay(100);
-        analogWrite(green, LOW);
-        delay(100);
-        analogWrite(blue, 11);
-        delay(100);
-        analogWrite(blue, LOW);
-        delay(100);
-        analogWrite(red, 14);
-        delay(100);
-        analogWrite(red, LOW);
-        delay(1000);
-      }
+    int ledVal = map(photoResistorStatus, 300, 800, 0, 200);
+    //Serial.println(photoResistorStatus);
+    //Serial.println(ledVal);
+    Serial.println("case 2 is working");
+    if (photoResistorStatus <= 450)
+    {
+      analogWrite(yellow, 5);
+      delay(100);
+      analogWrite(yellow, LOW);
+      delay(100);
+      analogWrite(green, ledVal);
+      delay(100);
+      analogWrite(green, LOW);
+      delay(100);
+      analogWrite(blue, 11);
+      delay(100);
+      analogWrite(blue, LOW);
+      delay(100);
+      analogWrite(red, 14);
+      delay(100);
+      analogWrite(red, LOW);
+      delay(1000);
+    }
   }
   else if (pattern == 3)
   {
-   allOff();
+    allOff();
   }
 }
 // void loop()
